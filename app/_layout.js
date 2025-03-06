28ba18fa16cc04a85594644e9dbc1e7c3db14224
Sahiljeet singh kalsi <sahiljeetsinghkalsi@gmail.com>
2025-03-06 22:59:08 +0530

    add layout component with font loading and splash screen handling; enhance PopularJobs and Welcome components with improved UI and functionality; add axios dependency; create custom useFetch hook

--- app/_layout.js
+++ app/_layout.js
@@ -1,7 +1,31 @@
-import {Stack} from "expo-router"
+import { Stack } from "expo-router";
+import { useCallback } from "react";
+import { useFonts } from "expo-font";
+import * as SplashScreen from 'expo-splash-screen';
+import { View } from "react-native";
 
-const layout = ()=>{
-    return <Stack/>
-}
+SplashScreen.preventAutoHideAsync();
 
-export default layout;
+const Layout = () => {
+    const [fontsLoaded] = useFonts({
+        DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+        DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+        DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+    });
+
+    const onLayoutRootView = useCallback(async () => {
+        if (fontsLoaded) {
+            await SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded]);
+
+    if (!fontsLoaded) return null;
+
+    return (
+        <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+            <Stack />
+        </View>
+    );
+};
+
+export default Layout;
