8a561ddb37e91f0908a54fec473a1f3e1623455c
Sahiljeet singh kalsi <sahiljeetsinghkalsi@gmail.com>
2025-03-08 17:05:09 +0530

    implement custom useFetch hook for API data retrieval; update axios dependency to version 1.8.2

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,2 +1,34 @@
 import { useEffect, useState } from "react";
+import axios from "axios"
 
+const useFetch = ()=>{
+    const [data, setData] = useState([])
+    const [isLoading,setIsLoading] = useState(false);
+    const [error,setError] = useState(null)
+    const axios = require('axios');
+
+const options = {
+  method: 'GET',
+  url: 'https://jsearch.p.rapidapi.com/estimated-salary',
+  params: {
+    job_title: 'python',
+    location_type: 'ANY',
+    years_of_experience: 'ALL'
+  },
+  headers: {
+    'x-rapidapi-key': 'd6ae0d57f9msh55fb1ae1ec6f3f2p1e474ejsna390869f234f',
+    'x-rapidapi-host': 'jsearch.p.rapidapi.com'
+  }
+};
+
+async function fetchData() {
+	try {
+		const response = await axios.request(options);
+		console.log(response.data);
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+fetchData();
+}
